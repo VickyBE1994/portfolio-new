1d9d0a6ca0def64969fd022ae45d60637d2f6b24
VickyBE1994 <vickyvip81@gmail.com>
2024-09-10 00:42:48 +0530

    new portfolio

---
+++ portfolio/src/constants/index.js
@@ -0,0 +1,71 @@
+import project1 from "../assets/assets/projects/project-1.png";
+import project2 from "../assets/assets/projects/project-2.jpg";
+import project3 from "../assets/assets/projects/project-3.png";
+import project4 from "../assets/assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and , as well as back-end technologies like Node.js, MySQL,  and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges.I complete a Full stack Developer course at  GUVI is an IIT-M & IIM-A incubated Ed-tech company that focuses on
+              providing personalized learning solutions for its learners right
+              from online learning, upskilling & recruitment opportunities in
+              world-class quality.`;
+
+   
+              
+
+export const PROJECTS = [
+  {
+    title: "FOOD DEL APP",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    links:[
+       { Github:"https://food-del-theta.vercel.app/",
+      live:"https://github.com/VickyBE1994/FOOD-DEL"}
+    ]
+  
+  },
+  {
+    title: "Real Estate App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Firebase","mongoDb"],
+    links:[
+      { Github:"https:github.com/VickyBE1994/REAL-ESTATE",
+     live:"https://real-estate-u7hy.vercel.app/"}
+   ]
+  },
+  
+  {
+    title: "Spotify Clone",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    links:[
+      { Github:"https://github.com/VickyBE1994/spotify-clone",
+     live:"https://spotify-clone-qo6v.vercel.app/"}
+   ]
+ 
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS",  "Express", "mongoDb"],
+    links:[
+      { Github:"https:github.com/VickyBE1994/BLog-App",
+     live:"https:blog-app-vt0v.onrender.com/"}
+   ]
+  },
+  
+];
+
+export const CONTACT = {
+  address: "Sholinganallur Chennai-97  ",
+  phoneNo: "+91 90 47 44 34 17 ",
+  email: "vickyvip81@gmail.com",
+};
